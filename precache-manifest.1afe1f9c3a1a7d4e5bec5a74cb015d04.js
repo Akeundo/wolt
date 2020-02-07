877dbf3758505ad988be2452ede83eaed6ebb21c
Akeundo <48980681+Akeundo@users.noreply.github.com>
2020-02-07 20:05:16 +0200

    Updates

---
+++ precache-manifest.1afe1f9c3a1a7d4e5bec5a74cb015d04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3ef8f893ce66da9f339e6b9da5e6db4",
+    "url": "/wolt/index.html"
+  },
+  {
+    "revision": "3307e35d78a037505f40",
+    "url": "/wolt/static/css/main.f029738a.chunk.css"
+  },
+  {
+    "revision": "7b16d4927bf472ecd4b2",
+    "url": "/wolt/static/js/2.8d09a402.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/wolt/static/js/2.8d09a402.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3307e35d78a037505f40",
+    "url": "/wolt/static/js/main.a1b2331c.chunk.js"
+  },
+  {
+    "revision": "95677ac071f8ae268fb7",
+    "url": "/wolt/static/js/runtime-main.cdc8cc74.js"
+  }
+]);
